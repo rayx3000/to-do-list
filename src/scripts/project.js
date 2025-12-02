5e16afd739950b98d3089c189d34d68b0471715a
rayx3000 <candole2020@gmail.com>
2025-12-02 16:50:09 +0800

    add dialog form

--- src/scripts/project.js
+++ src/scripts/project.js
@@ -8,15 +8,13 @@ class Project {
     }
 }
 
-const defaultProject = new Project("Default", "proj-001");
-const minecraftProject = new Project("Build House In Minecraft", "proj-002");
-const basketballProject = new Project("Practice and Mastering Basketball", "proj-003");
+const projects = [new Project("Default", "proj-001"), new Project("Build House In Minecraft", "proj-002"), new Project("Practice and Mastering Basketball", "proj-003")];
 
-defaultProject.content = [
+projects[0].content = [
     new ToDoList(
         "89d102c7-e468-4067-9214-72aa5d4c82df", 
         "Playing Games", 
-        defaultProject.name, 
+        projects[0].name, 
         "12-20-2025", 
         "Low", 
         "It is Just For Fun and Activities"
@@ -24,7 +22,7 @@ defaultProject.content = [
     new ToDoList(
         "f5b3b2d6-a177-42ad-973f-5a76a55ab2db", 
         "Buy T-Shirt", 
-        defaultProject.name, 
+        projects[0].name, 
         "12-21-2025", 
         "High", 
         "To make house cleaner"
@@ -32,14 +30,13 @@ defaultProject.content = [
     new ToDoList(
         "2938d67f-10a7-4c98-86bf-18793cf98b13", 
         "Playing Games", 
-        defaultProject.name, 
+        projects[0].name, 
         "12-22-2025", 
         "Medium", 
         "Go to shop to buy more"
     ),
 ];
 
-const projects = [defaultProject, minecraftProject, basketballProject];
 
 console.log(projects[0].content);
 
@@ -50,3 +47,10 @@ projects[0].content.map((task) => {
 })
 
 document.querySelector(".to-do-list").innerHTML = content;
+
+const addDialog = document.querySelector(".add-dialog");
+const addTaskBtn = document.querySelector(".add-task-btn");
+const closeDialog = document.getElementById("close");
+
+addTaskBtn.addEventListener("click", () => addDialog.showModal());
+closeDialog.addEventListener("click", () => addDialog.close());
